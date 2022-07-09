6dded9e5f66796d65a04c919b35d7b4fa2f98742
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-09 08:19:11 +0530

    done

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+app.use(express.static(__dirname + '/public_final'));
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index_99.html"));
+//   res.render(path.join(__dirname + "/index.html"));
+  // _dirname : It will resolve to your project folder.
+});
+
+router.get("/1", function(req,res){
+  // app.use(express.static(__dirname + '/public2')); 
+  res.sendFile(path.join(__dirname + "/index_100.html"))
+})
+
+router.get("/2", function(req,res){
+  // app.use(express.static(__dirname + '/public2')); 
+  res.sendFile(path.join(__dirname + "/index_101.html"))
+})
+
+router.get("/3", function(req,res){
+  // app.use(express.static(__dirname + '/public2')); 
+  res.sendFile(path.join(__dirname + "/index_102.html"))
+})
+
+// add the router
+app.use('/',router);
+app.listen(process.env.part || 3000);
+
+console.log('Running at port 3000')
