6f499a8afc0a494ec9ca1b7b30c125edf98f14a9
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-07-09 08:55:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,22 +6,28 @@ const router = express.Router();
 app.use(express.static(__dirname + '/public_final'));
 
 router.get("/", function (req, res) {
+  res.send("Hello world");
+//   res.render(path.join(__dirname + "/index.html"));
+  // _dirname : It will resolve to your project folder.
+});
+
+router.get("/1", function (req, res) {
   res.sendFile(path.join(__dirname + "/index_99.html"));
 //   res.render(path.join(__dirname + "/index.html"));
   // _dirname : It will resolve to your project folder.
 });
 
-router.get("/1", function(req,res){
+router.get("/2", function(req,res){
   // app.use(express.static(__dirname + '/public2')); 
   res.sendFile(path.join(__dirname + "/index_100.html"))
 })
 
-router.get("/2", function(req,res){
+router.get("/3", function(req,res){
   // app.use(express.static(__dirname + '/public2')); 
   res.sendFile(path.join(__dirname + "/index_101.html"))
 })
 
-router.get("/3", function(req,res){
+router.get("/4", function(req,res){
   // app.use(express.static(__dirname + '/public2')); 
   res.sendFile(path.join(__dirname + "/index_102.html"))
 })
